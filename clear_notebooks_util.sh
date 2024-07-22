bd290ad751a314221b96829b178aa7e3a448ce2d
daniel redder <daniel@redder.dev>
2024-07-22 10:25:45 -0400

    notebook cleaning utility

--- clear_notebooks_util.sh
+++ clear_notebooks_util.sh
@@ -1,2 +1,2 @@
 #based off of https://mg.readthedocs.io/git-jupyter.html#cleaning-a-whole-repository
-git filter-branch --tree-filter "find . -name "*.ipynb" -exec python3 -m nbconvert --ClearOutputPreprocessor.enabled=True --inplace {} || true"
+git filter-branch -f --tree-filter 'find . -name "*.ipynb" -exec python3 -m nbconvert --ClearOutputPreprocessor.enabled=True --inplace {} \; || true'
