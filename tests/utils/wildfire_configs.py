8e43a733b89d3aa60b1e2b7492159b7fc49a4cf0
C4 Patino <c4patino@gmail.com>
2024-08-07 04:16:33 +0000

    Changed test configuration to fit new configuration

--- tests/utils/wildfire_configs.py
+++ tests/utils/wildfire_configs.py
@@ -1,66 +1,79 @@
-from typing import Tuple
 import torch
 import numpy as np
 
-from free_range_zoo.free_range_zoo.envs.wildfire.env.structures.configuration import GridConf, StochasticConf
+from free_range_zoo.free_range_zoo.envs.wildfire.env.structures.configuration import (WildfireConfiguration,
+                                                                                      FireConfiguration,
+                                                                                      AgentConfiguration,
+                                                                                      StochasticConfiguration)
 
 
-def non_stochastic() -> Tuple[GridConf, StochasticConf]:
+def non_stochastic() -> WildfireConfiguration:
     """
     Create a non-stochastic configuration for the wildfire environment.
 
     Returns:
-        Tuple[GridConf, StochasticConf]: The grid configuration and the stochastic configuration.
+        WildfireConfiguration: The configuration.
     """
-    grid_conf = GridConf(
-        grid_width=3,
-        grid_height=2,
-        lit=torch.tensor([
-            [0, 0, 0],
-            [1, 1, 1]
-        ], dtype=torch.bool),
+
+    fire_configuration = FireConfiguration(
         fire_types=torch.tensor([
             [0, 0, 0],
             [1, 2, 1]
         ], dtype=torch.int32),
+        num_fire_states=5,
         fire_rewards=torch.tensor([
             [0, 0, 0],
             [20.0, 50.0, 20.0]
         ], dtype=torch.float32),
+        lit=torch.tensor([
+            [0, 0, 0],
+            [1, 1, 1]
+        ], dtype=torch.bool),
+        intensity_increase_probability=1.0,
+        intensity_decrease_probability=1.0,
+        burnout_probability=1.0,
+
+        base_spread_rate=3.0,
+        max_spread_rate=67.0,
+        random_ignition_probability=0.0,
+        cell_size=200.0,
+        wind_direction=0.0 * np.pi,
         ignition_temp=torch.tensor([
             [2, 2, 2],
             [2, 2, 2]
         ], dtype=torch.int32),
-        burnable_tiles=((1, 0), (1, 1), (1, 2)),
-        num_fire_states=5,
         initial_fuel=2,
-        base_spread=3.0,
-        max_spread_rate=67.0,
-        cell_size=200.0,
-        wind_direction=0.0 * np.pi,
+    )
 
-        agents=torch.tensor([
-            [1, 3, 2],
-            [0, 0, 0]
-        ], dtype=torch.int32),
-        attack_range=torch.tensor([1, 1, 1], dtype=torch.int32),
+    agent_configuration = AgentConfiguration(
+        agents=torch.tensor([[0, 0], [0, 1], [0, 2]], dtype=torch.int32),
         fire_reduction_power=torch.tensor([1, 1, 1], dtype=torch.int32),
-        init_suppressant=2,
+        fire_reduction_power_per_extra_agent=0.0,
+        attack_range=torch.tensor([1, 1, 1], dtype=torch.int32),
+
         suppressant_states=3,
+        initial_suppressant=2,
+        suppressant_decrease_probability=1.0,
+        suppressant_refill_probability=1.0
     )
 
-    stochastic_conf = StochasticConf(
-        suppressant_decrease=False,
+    stochastic_configuration = StochasticConfiguration(
+        special_burnout_probability=False,
+
         suppressant_refill=False,
-        BASE_FIRE_REDUCTION=1.0,
-        FIRE_REDUCTION_PER_EXTRA_AGENT=0.0,
+        suppressant_decrease=False,
 
-        use_fire_spread=False,
-        use_random_fire_ignition=False,
-        use_almost_burned_out=False,
-        use_fire_fuel=False,
+        fire_spread=False,
         realistic_fire_spread=False,
-        FIRE_INTENSITY_INCREASE_PROBABILITY=1.0
+        random_fire_ignition=False,
+        fire_fuel=False,
     )
 
-    return grid_conf, stochastic_conf
+    return WildfireConfiguration(
+        grid_width=3,
+        grid_height=2,
+
+        fire_config=fire_configuration,
+        agent_config=agent_configuration,
+        stochastic_config=stochastic_configuration
+    )
