611f75fa17d8e72812bb2b246aa0fd7302f45d42
C4 Patino <c4patino@gmail.com>
2024-11-08 10:38:40 -0600

    Fixed environment tests, added GPU CI/CD
    fixing unittest
    
    Testing github self-hosted workflow
    
    Added GPU CI/CD
    
    Trying this
    
    Test

--- docs/free_range_zoo_docs/__init__.py
+++ docs/free_range_zoo_docs/__init__.py
@@ -1,5 +1,6 @@
 """This module contains functions to generate and build the documentation and parsing."""
 import subprocess
+import os
 
 
 def gen():
@@ -16,14 +17,32 @@ def sync():
 
 def build():
     """Build the documentation."""
-    process = subprocess.run(["make", "html"])
+    makefile_dir = None
+    for root, dirs, files in os.walk(os.getcwd()):
+        if '.venv' in root or '.git' in root or '__pycache__' in root:
+            continue
+
+        if "Makefile" in files:
+            makefile_dir = root
+            break
+
+    process = subprocess.run(["make", "html"], cwd=makefile_dir)
     if process.returncode != 0:
         raise RuntimeError("Failed to build the documentation.")
 
 
 def watch():
     """Build the documentation and watch for changes."""
-    subprocess.run(["sphinx-autobuild", "-b", "html", "./source", "_build"])
+    makefile_dir = None
+    for root, dirs, files in os.walk(os.getcwd()):
+        if '.venv' in root or '.git' in root or '__pycache__' in root:
+            continue
+
+        if "Makefile" in files:
+            makefile_dir = root
+            break
+
+    subprocess.run(["sphinx-autobuild", "-b", "html", "./source", "_build"], cwd=makefile_dir)
 
 
 def test():
