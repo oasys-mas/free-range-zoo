c613e930601a7a24e7565c5f0abc247d236150d5
C4 Patino <c4patino@gmail.com>
2024-11-19 18:38:31 -0600

    Added base imports for wildfire and cybersecurity baselines

---
+++ free_range_zoo/envs/wildfire/baselines/__init__.py
@@ -0,0 +1,4 @@
+from free_range_zoo.envs.wildfire.baselines.noop import RandomBaseline
+from free_range_zoo.envs.wildfire.baselines.random import NoopBaseline
+from free_range_zoo.envs.wildfire.baselines.strongest import StrongestBaseline
+from free_range_zoo.envs.wildfire.baselines.weakest import WeakestBaseline
