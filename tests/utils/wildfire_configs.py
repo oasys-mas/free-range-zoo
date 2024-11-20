6d7c4c696ea3e975bc0c107b4b24e4b9c50579c1
C4 Patino <c4patino@gmail.com>
2024-11-19 18:37:07 -0600

    Removed termination reward from test

--- tests/utils/wildfire_configs.py
+++ tests/utils/wildfire_configs.py
@@ -18,7 +18,7 @@ def non_stochastic() -> WildfireConfiguration:
         fire_rewards=torch.tensor([[0, 0, 0], [20.0, 50.0, 20.0]], dtype=torch.float32),
         bad_attack_penalty=-100.0,
         burnout_penalty=-1.0,
-        termination_reward=1000.0,
+        termination_reward=0.0,
     )
 
     fire_configuration = FireConfiguration(
