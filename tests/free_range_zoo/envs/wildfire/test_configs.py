79a3868edcaff34a43bb38ecf1a8de7efa4aba98
C4 Patino <c4patino@gmail.com>
2025-01-06 16:21:05 -0600

    Removed planning test configs

--- tests/free_range_zoo/envs/wildfire/test_configs.py
+++ tests/free_range_zoo/envs/wildfire/test_configs.py
@@ -1,19 +1,6 @@
 import unittest
 
-from free_range_zoo.envs.wildfire.configs.planning import setup3, single_agent_two_fires, single_agent_single_fire
 from free_range_zoo.envs.wildfire.configs.aaai_2024 import aaai_2025_ol_config
-
-
-class TestPlanningConfigurationInitialization(unittest.TestCase):
-
-    def test_setup3(self) -> None:
-        setup3()
-
-    def test_single_agent_two_fires(self) -> None:
-        single_agent_two_fires()
-
-    def test_single_agent_single_fire(self) -> None:
-        single_agent_single_fire()
 
 
 class TestAAAI2024ConfigurationInitialization(unittest.TestCase):
