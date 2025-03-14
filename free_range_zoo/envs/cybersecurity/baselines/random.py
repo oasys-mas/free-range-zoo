4dd967f7e46850eeed234f4c0d44c6a6f52a0428
C4 Patino <c4patino@gmail.com>
2025-03-14 13:43:39 -0500

    fix(cybersecurity): updated random baseline so that it returns a tensor

--- free_range_zoo/envs/cybersecurity/baselines/random.py
+++ free_range_zoo/envs/cybersecurity/baselines/random.py
@@ -1,5 +1,5 @@
 """Agent the samples actions available to it in a uniform distribution."""
-from typing import List
+import torch
 import free_range_rust
 from free_range_zoo.utils.agent import Agent
 
@@ -7,13 +7,13 @@ from free_range_zoo.utils.agent import Agent
 class RandomBaseline(Agent):
     """Agent that samples actions avaialable to it in a uniform distribution."""
 
-    def act(self, action_space: free_range_rust.Space) -> List[List[int]]:
+    def act(self, action_space: free_range_rust.Space) -> torch.IntTensor:
         """
         Return a list of actions, one for each parallel environment.
 
         Args:
             action_space: free_range_rust.Space - Current action space available to the agent.
         Returns:
-            List[List[int]] - List of actions, one for each parallel environment.
+            torch.IntTensor - List of actions, one for each parallel environment.
         """
-        return action_space.sample_nested()
+        return torch.tensor(action_space.sample_nested(), dtype=torch.int32)
