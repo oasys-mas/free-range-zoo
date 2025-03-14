172578251328ab43c1332d131591dc688162d4a4
daniel redder <daniel@redder.dev>
2025-03-14 14:49:42 -0400

    casting randomly sampled actions to tensors for wildfire.

--- free_range_zoo/envs/wildfire/baselines/random.py
+++ free_range_zoo/envs/wildfire/baselines/random.py
@@ -2,18 +2,18 @@
 from typing import List
 import free_range_rust
 from free_range_zoo.utils.agent import Agent
-
+import torch
 
 class RandomBaseline(Agent):
     """Agent that samples actions avaialable to it in a uniform distribution."""
 
-    def act(self, action_space: free_range_rust.Space) -> List[List[int]]:
+    def act(self, action_space: free_range_rust.Space) -> torch.IntTensor:
         """
         Return a list of actions, one for each parallel environment.
 
         Args:
-            action_space: free_range_rust.Space - Current action space available to the agent.
+            action_space (free_range_rust.Space): Current action space available to the agent.
         Returns:
-            List[List[int]] - List of actions, one for each parallel environment.
+            (torch.IntTensor): Tensor of actions, one for each parallel environment.
         """
-        return action_space.sample_nested()
+        return torch.tensor(action_space.sample_nested(), dtype=torch.int32)
