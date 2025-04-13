7fe5f6feaeb58eb3da26537efcb73db2c328b456
daniel redder <daniel@redder.dev>
2025-04-13 11:09:15 -0400

    bugfix, added parameter to space_validator.py to allow for task-agnostic actions used on a specific task.

--- free_range_zoo/wrappers/space_validator.py
+++ free_range_zoo/wrappers/space_validator.py
@@ -16,7 +16,7 @@ class ActionSpaceValidatorModifier(BaseModifier):
     env = True
     subject_agent = True
 
-    def __init__(self, env: BatchedAECEnv, subject_agent: str, collapse: bool = False):
+    def __init__(self, env: BatchedAECEnv, subject_agent: str, collapse: bool = False, allow_flexible_task_tags: bool = True):
         """
         Initialize the ActionSpaceValidatorModifier.
 
@@ -24,8 +24,10 @@ class ActionSpaceValidatorModifier(BaseModifier):
             env: BatchedAECEnv - The environment to wrap.
             subject_agent: str - The subject agent of the graph wrapper.
             collapse: bool - Whether to collapse the task-action and task-agnostic action nodes into single nodes.
+            allow_flexible_task_tags: bool - Whether to allow task-agnostic actions to be used for any task instead of just the task-agnostic task.
         """
         self.env = env
+        self.allow_flexible_task_tags = allow_flexible_task_tags
 
         # Unpack the the parallel environment if it is wrapped in one.
         if hasattr(self.env, 'aec_env'):
@@ -49,21 +51,25 @@ class ActionSpaceValidatorModifier(BaseModifier):
         for index, (action, space) in enumerate(zip(actions_iter, action_spaces)):
             task_channel, action_channel = action.squeeze(0)
 
-            try:
-                discrete = space.spaces[task_channel]
-            except IndexError as e:
-                logger.critical(
-                    f'{self.subject_agent} in batch {index} attempted to take an action on a undefined task.\nAction: %s\nSpace: %s',
-                    action, space)
-                raise e
-            try:
-                if action_channel < discrete.start or action_channel > discrete.start + discrete.n:
+            # If the action is a task-agnostic action, we need to check if it is within the bounds of the action space.
+            if action_channel < 0 and self.allow_flexible_task_tags:
+                # in_any_task = any([action_channel > space.start and action_channel < space.start + space.n for space in space.spaces])
+                task_agnostic_task = space.spaces[-1]
+                if action_channel < task_agnostic_task.start or action_channel > task_agnostic_task.start + task_agnostic_task.n:
+                    logger.critical(f'{self.subject_agent} in batch {index} attempted to take an task-agnostic action that is not defined.\nAction: %s\nSpace: %s',action, task_agnostic_task)
                     raise IndexError
-            except IndexError as e:
-                logger.critical(
-                    f'{self.subject_agent} in batch {index} attempted to take an action that is not defined for a defined task.\nAction: %s\nSpace: %s',
-                    action, space)
-                raise e
+            else:
+                try:
+                    discrete = space.spaces[task_channel]
+                except IndexError as e:
+                    logger.critical(f'{self.subject_agent} in batch {index} attempted to take an action on a undefined task.\nAction: %s\nSpace: %s',action, space)
+                    raise e
+                try:
+                    if action_channel < discrete.start or action_channel > discrete.start + discrete.n:
+                        raise IndexError
+                except IndexError as e:
+                    logger.critical(f'{self.subject_agent} in batch {index} attempted to take an action that is not defined for a defined task.\nAction: %s\nSpace: %s',action, space)
+                    raise e
 
         return actions
 
