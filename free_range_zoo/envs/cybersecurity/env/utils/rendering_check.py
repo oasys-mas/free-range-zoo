b815fb7dba8f1b49881dc9e0ade7ffa2dc0dea57
Bboyfury <Alirezasalehabadi.as@gmail.com>
2026-01-22 11:46:47 -0600

    fix cybersec, agents bad positionings.

--- free_range_zoo/envs/cybersecurity/env/utils/rendering_check.py
+++ free_range_zoo/envs/cybersecurity/env/utils/rendering_check.py
@@ -4,7 +4,7 @@ from rendering import render
 
 if __name__ == "__main__":
     # Path to the CSV file
-    csv_path = "/home/alire/repos_wsl/dev-free-range-zoo/outputs/wildfire_logging_test_0/0.csv"
+    csv_path = "/home/alire/repos_wsl/dev-free-range-zoo/outputs/cyberSec_logging_test_0/eck.csv"
 
     # Render mode options: 'human' or 'rgb_array'
     render_mode = "rgb_array"  # Change to "rgb_array" if you want frames
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     # frames = frames[::-1]  # Reverse the frames if they appear reversed in the GIF
     # Save frames as a GIF
-    gif_path = "wildfire_simulation.gif"
+    gif_path = "cybersecurity_simulation.gif"
     imageio.mimsave(gif_path, frames, fps=0.5)
     render_mode = "human"  # Change to "rgb_array" if you want frames
 
